931a4be5dafac8fbc02bdba04d65d6e50f9e7670
Lars Jarred Speetjens <lars@speetjens.net>
2026-06-30 15:37:25 +0200

    feat: add DCO License link to about page

--- web/app/pages/about.vue
+++ web/app/pages/about.vue
@@ -74,6 +74,18 @@
             clip-rule="evenodd" />
         </svg>
       </a>
+
+      <a href="https://github.com/larsjarred9/Pawbby-Reborn/blob/website/CONTRIBUTING.md" target="_blank"
+        class="w-full flex items-center justify-between text-white/90 hover:text-white group">
+        <span class="font-medium text-lg">DCO License</span>
+        <svg xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5 text-pawbby-mutedDark group-hover:text-pawbby-muted transition-colors" viewBox="0 0 20 20"
+          fill="currentColor">
+          <path fill-rule="evenodd"
+            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+            clip-rule="evenodd" />
+        </svg>
+      </a>
     </div>
 
     <!-- Footer Branding -->
