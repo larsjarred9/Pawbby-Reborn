5a2aee67fb466d52f0a4a0ab9da0b05b883e80f4
Lars Jarred Speetjens <lars@speetjens.net>
2026-07-01 09:14:52 +0200

    refactor: move port configuration to environment variables in ecosystem config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,13 +2,13 @@ module.exports = {
   apps: [
     {
       name: 'pawbby',
-      port: '3000',
       cwd: './web',
       script: './.output/server/index.mjs',
       exec_mode: 'fork',
       instances: 1,
       env: {
         NODE_ENV: 'production',
+        PORT: 3000
       }
     }
   ]
