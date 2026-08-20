abc04dc289a02e81e693d756fbc556f30c5d2e1c
Sharlet Kurien <sharletman@gmail.com>
2026-08-20 08:22:24 +0300

    chore: Consistent file formatting.

--- web/server/utils/prisma.ts
+++ web/server/utils/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaLibSql } from '@prisma/adapter-libsql'
 import { PrismaClient } from '@prisma/client'
 
-const dbUrl = process.env.DATABASE_URL || "file:./dev.db"
+const dbUrl = process.env.DATABASE_URL || 'file:./dev.db'
 const adapter = new PrismaLibSql({ url: dbUrl })
 
 export const prisma = new PrismaClient({ 
